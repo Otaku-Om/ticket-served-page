db24338e26cff26d8adbb6774bce032c848750f4
om <saxenaom12@gmail.com>
2024-12-03 12:24:48 +0530

    root url

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 // App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import TicketPage from './TicketPage';
+import TicketPage from './components/TicketPage';
+import Home from './components/Home';
 
 function App() {
   return (
     <Router>
       <Routes>
         {/* Route for ticket page with dynamic ticket_number */}
+        <Route path="/" element={<Home />} />
         <Route path="/ticket/:ticket_number" element={<TicketPage />} />
       </Routes>
     </Router>
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='center-contain'>
+      <h2 style={{fontSize: "50px",}}>Home Ticket</h2>
+    </div>
+  )
+}
+
+export default Home
